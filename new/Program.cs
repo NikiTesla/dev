fdfa23770d508dd0bb7f634dc05b08ab8d4b3cae
Nikita <krechetov.n@outlook.com>
2022-10-19 11:55:40 +0300

    testing functions

--- new/Program.cs
+++ new/Program.cs
@@ -6,9 +6,30 @@ namespace devdotnet.New
 {
     public class Program
     {
+        struct Point
+        {
+            public double x;
+            public double y;
+            public double z;
+            public Point(double x, double y, double z)
+            {
+                this.x = x; this.y = y; this.z = z;
+            }
+        }
+
+        static double Sum(string message, params double[] parameters)
+        {
+            double result = 0;
+            Console.Write(message);
+
+            for (int i = 0; i < parameters.Length; i++) result += parameters[i];
+
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Sum("Hello, sum is: ", 1.1, 2.2, 3.0, 4.1));
         }
     }
 }
