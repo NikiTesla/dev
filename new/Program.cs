93e36de9d564cbe167eb0ea8d5df5272c5a70c04
Nikita <krechetov.n@outlook.com>
2022-10-19 11:51:47 +0300

    added new console_project

---
+++ new/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace devdotnet.New
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
