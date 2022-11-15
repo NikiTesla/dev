a22629773358aefadddc2be1866b5e6814da8838
Nikita <krechetov.n@outlook.com>
2022-11-15 09:46:31 +0300

    changed site for anna

---
+++ q_app/app/api/test.py
@@ -0,0 +1,65 @@
+import threading
+import requests
+import argparse
+
+import sys
+sys.path.append('/home/krechetov/dev/q_app')
+
+from flask import Flask, request, jsonify, abort
+from utils import config_parser
+from app.db.exceptions import UserNotFoundException
+from app.db.interaction.interaction import DbInteraction
+
+class Server:
+
+    def __init__(self, host, port, ebuild_db=False):
+        self.host = host
+        self.port = port
+
+        self.app = Flask(__name__)
+        self.app.add_url_rule("/shutdown", view_func=self.shutdown)
+        self.app.add_url_rule("/", view_func=self.get_home)
+        self.app.add_url_rule("/home", view_func=self.shutdown)
+
+        self.app.register_error_handler(404, self.page_not_found)
+
+
+    def page_not_found(self, err_description):
+        return jsonify(error=str(err_description)), 404
+
+
+    def run_server(self):
+        self.server = threading.Thread(target=self.app.run, kwargs={'host' : self.host, 'port' : self.port})
+        self.server.start()
+        return self.server
+
+    def shutdown_server(self):
+        request.get(f'http://{self.host}:{self.port}/shutdown')
+
+    def shutdown(self):
+        terminate_func = request.environ.get('werkzeug.server.shutdown')
+        if terminate_func:
+            terminate_func()
+
+    def get_home(self):
+        return "Hello, api server"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, dest='config')
+
+    args = parser.parse_args()
+
+    config = config_parser(args.config)
+
+    server_host = config['SERVER_HOST']
+    server_port = config['SERVER_PORT']
+
+
+    server = Server(
+        host = server_host,
+        port = server_port     
+    )
+
+    server.run_server()
